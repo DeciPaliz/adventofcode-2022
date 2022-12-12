2fc5325c622640c1741524fe0fdc3152ead48d81
DeciPaliz <unat7919@gmail.com>
2022-12-13 01:43:08 +0500

    8

--- src/bin/08_1.rs
+++ src/bin/08_1.rs
@@ -1,5 +1,37 @@
 use std::io;
 
+fn check_visibility(i: usize, j: usize, map: &Vec<Vec<u8>>) -> bool {
+    if i == 0 || j == 0 || i == map.len() - 1 || j == map[0].len() - 1 { return true }
+    let row = map[i].clone();
+    let col = map.iter().map(|row| row[j]).collect::<Vec<u8>>();
+    let up_max = *col[..i].iter().max().unwrap();
+    let down_max = *col[i+1..].iter().max().unwrap();
+    let left_max = *row[..j].iter().max().unwrap();
+    let right_max = *row[j+1..].iter().max().unwrap();
+    let val = map[i][j];
+    val > up_max || val > down_max || val > left_max || val > right_max
+}
+
 fn main() -> io::Result<()> {
+    let stdin = io::stdin();
+    let mut map: Vec<Vec<u8>> = Vec::new();
+
+    for line in stdin.lines() {
+        map.push(line?.trim().chars().map(|x| x.to_digit(10).unwrap() as u8).collect::<Vec<u8>>());
+    }
+
+    let mut count = 0;
+    
+    let rows = map.len();
+    let cols = map[0].len();
+
+    for i in 0..rows {
+        for j in 0..cols {
+            if check_visibility(i, j, &map) { count += 1 }
+        }
+    }
+
+    println!("{}", count);
+
     Ok(())
 }
--- src/bin/08_2.rs
+++ src/bin/08_2.rs
@@ -1,5 +1,70 @@
 use std::io;
 
+fn get_scenic_score(i: usize, j: usize, map: &Vec<Vec<u8>>) -> u32 {
+    let row = map[i].clone();
+    let col = map.iter().map(|row| row[j]).collect::<Vec<u8>>();
+    let val = map[i][j];
+
+    let mut up_score = 0;
+    let mut down_score = 0;
+    let mut left_score = 0;
+    let mut right_score = 0;
+
+    for &tree in col[..i].iter().rev() {
+        up_score += 1;
+        if tree >= val {
+            break;
+        }
+    }
+    if up_score == 0 { return 0 }
+
+    for &tree in col[i+1..].iter() {
+        down_score += 1;
+        if tree >= val {
+            break;
+        }
+    }
+    if down_score == 0 { return 0 }
+
+    for &tree in row[..j].iter().rev() {
+        left_score += 1;
+        if tree >= val {
+            break;
+        }
+    }
+    if left_score == 0 { return 0 }
+
+    for &tree in row[j+1..].iter() {
+        right_score += 1;
+        if tree >= val {
+            break;
+        }
+    }
+    if right_score == 0 { return 0 }
+
+    up_score * down_score * left_score * right_score
+}
+
 fn main() -> io::Result<()> {
+    let stdin = io::stdin();
+    let mut map: Vec<Vec<u8>> = Vec::new();
+
+    for line in stdin.lines() {
+        map.push(line?.trim().chars().map(|x| x.to_digit(10).unwrap() as u8).collect::<Vec<u8>>());
+    }
+
+    let mut max_score: u32 = 0;
+
+    let rows = map.len();
+    let cols = map[0].len();
+
+    for i in 1..rows-1 {
+        for j in 1..cols-1 {
+            max_score = max_score.max(get_scenic_score(i, j, &map));
+        }
+    }
+
+    println!("{}", max_score);
+
     Ok(())
 }
