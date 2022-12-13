63b75b8f84a475a0d686c6841fa64f4065abab9e
DeciPaliz <unat7919@gmail.com>
2022-12-13 17:59:45 +0500

    10

--- src/bin/10_1.rs
+++ src/bin/10_1.rs
@@ -1,5 +1,37 @@
 use std::io;
 
 fn main() -> io::Result<()> {
+    let stdin = io::stdin();
+
+    let mut x = 1;
+    let mut sum = 0;
+    let mut cycle = 1;
+
+    for line in stdin.lines() {
+        let line = line?;
+        let mut split = line.split(' ');
+        let command = split.next().unwrap().trim();
+        let amount: Option<i32> = if let Some(s) = split.next() { Some(s.trim().parse::<i32>().unwrap()) } else { None };
+
+        if cycle >= 20 && (cycle - 20) % 40 == 0 {
+            sum += x * cycle;
+        }
+        
+        match command {
+            "noop" => { cycle += 1 },
+            "addx" => { 
+                cycle += 1;
+                if cycle >= 20 && cycle <= 220 && (cycle - 20) % 40 == 0 {
+                    sum += x * cycle;
+                }
+                cycle += 1;
+                x += amount.unwrap();
+            },
+            _ => panic!()
+        }
+    }
+
+    println!("{}", sum);
+
     Ok(())
 }
--- src/bin/10_2.rs
+++ src/bin/10_2.rs
@@ -1,5 +1,47 @@
 use std::io;
 
 fn main() -> io::Result<()> {
+    let stdin = io::stdin();
+
+    let mut x = 1;
+    let mut cycle = 0;
+    let mut crt_row = String::new();
+    let mut crt = String::new();
+
+    for line in stdin.lines() {
+        let line = line?;
+        let mut split = line.split(' ');
+        let command = split.next().unwrap().trim();
+        let amount: Option<i32> = if let Some(s) = split.next() { Some(s.trim().parse::<i32>().unwrap()) } else { None };
+
+        if cycle % 40 == 0 {
+            crt.push_str(&crt_row);
+            crt.push('\n');
+            crt_row = String::new();
+        }
+
+        crt_row.push(if (cycle % 40 as i32).abs_diff(x) <= 1 { '#' } else { '.' });
+        
+        match command {
+            "noop" => { cycle += 1 },
+            "addx" => { 
+                cycle += 1;
+
+                if cycle % 40 == 0 {
+                    crt.push_str(&crt_row);
+                    crt.push('\n');
+                    crt_row = String::new();
+                }
+                crt_row.push(if (cycle % 40 as i32).abs_diff(x) <= 1 { '#' } else { '.' });
+
+                cycle += 1;
+                x += amount.unwrap();
+            },
+            _ => panic!()
+        }
+    }
+
+    println!("{}{}\n", crt, crt_row);
+
     Ok(())
 }
